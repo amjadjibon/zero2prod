3ad584dcdb825d8ee028a5ba2394ff8c79de8834
Amjad Jibon <amjad.jibon@gmail.com>
2023-03-18 19:46:01 +0600

    help api and integration test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8080")
+        .expect("failed to bind port");
+    run(listener)?.await
 }
